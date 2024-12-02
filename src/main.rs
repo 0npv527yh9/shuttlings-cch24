12517cf619fac9dd3617dd416d53362acf3b441f
Junya Hirashima <jhirashima99@gmail.com>
2024-12-03 00:50:22 +0900

    feat: Solve Task2

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,28 @@
-use axum::{routing::get, Router};
+use axum::{
+    http::{header, HeaderName, StatusCode},
+    routing::get,
+    Router,
+};
 
 async fn hello_world() -> &'static str {
     "Hello, bird!"
 }
 
+async fn seek() -> (StatusCode, [(HeaderName, &'static str); 1]) {
+    (
+        StatusCode::FOUND,
+        [(
+            header::LOCATION,
+            "https://www.youtube.com/watch?v=9Gc4QTqslN4",
+        )],
+    )
+}
+
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new().route("/", get(hello_world));
+    let router = Router::new()
+        .route("/", get(hello_world))
+        .route("/-1/seek", get(seek));
 
     Ok(router.into())
 }
