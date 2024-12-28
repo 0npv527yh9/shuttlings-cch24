a55ac8c0b2c0ba7107c0bab044cb4c46a1b2660e
Junya Hirashima <jhirashima99@gmail.com>
2024-12-28 10:38:37 +0900

    refactor

--- src/day5.rs
+++ src/day5.rs
@@ -3,8 +3,8 @@ pub mod task1 {
     use std::{fmt::Display, str::FromStr};
 
     use axum::http::StatusCode;
-    use cargo_manifest::Manifest;
-    use toml::{Table, Value};
+    use cargo_manifest::{Manifest, MaybeInherited};
+    use toml::Value;
 
     struct Order {
         item: String,
@@ -23,56 +23,71 @@ pub mod task1 {
             Err(_) => return (StatusCode::BAD_REQUEST, "Invalid manifest".to_string()),
         };
 
-        let is_valid_keywords = match (|| manifest.package?.keywords?.as_local())() {
+        let is_valid_keywords = match get_keywords(&manifest) {
             Some(keywords) => keywords.contains(&"Christmas 2024".to_string()),
             None => false,
         };
 
         if !is_valid_keywords {
-        return (
+            return (
                 StatusCode::BAD_REQUEST,
                 "Magic keyword not provided".to_string(),
             );
         }
 
-        let manifest = body.parse::<Table>().unwrap();
-
-        let parse = || -> Option<Vec<Order>> {
-            let orders = manifest
-                .get("package")?
-                .get("metadata")?
-                .get("orders")?
-                .as_array()?
-                .iter()
-                .filter_map(|order| {
-                    if let Value::Table(order) = order {
-                        if let (Some(Value::String(item)), Some(Value::Integer(quantity))) =
-                            (order.get("item"), order.get("quantity"))
-                        {
-                            return Some(Order {
-                                item: item.to_owned(),
-                                quantity: *quantity as u32,
-                            });
-                        }
-                    }
-                    None
-                })
-                .collect::<Vec<_>>();
-            Some(orders)
+        let get_valid_orders = || {
+            if let Some(orders) = get_orders(&manifest) {
+                if !orders.is_empty() {
+                    return Some(orders);
+                }
+            }
+            None
         };
 
-        if let Some(orders) = parse() {
-            if !orders.is_empty() {
-                return (
-                    StatusCode::OK,
-                    orders
-                        .iter()
-                        .map(Order::to_string)
-                        .collect::<Vec<_>>()
-                        .join("\n"),
-                );
-            }
+        if let Some(orders) = get_valid_orders() {
+            (
+                StatusCode::OK,
+                orders
+                    .iter()
+                    .map(Order::to_string)
+                    .collect::<Vec<_>>()
+                    .join("\n"),
+            )
+        } else {
+            (StatusCode::NO_CONTENT, String::new())
         }
-        (StatusCode::NO_CONTENT, String::new())
+    }
+
+    fn get_keywords(manifest: &Manifest) -> Option<&Vec<String>> {
+        match manifest.package.as_ref()?.keywords.as_ref()? {
+            MaybeInherited::Local(keywords) => Some(keywords),
+            _ => None,
+        }
+    }
+
+    fn get_orders(manifest: &Manifest) -> Option<Vec<Order>> {
+        let orders = manifest
+            .package
+            .as_ref()?
+            .metadata
+            .as_ref()?
+            .get("orders")?
+            .as_array()?
+            .iter()
+            .filter_map(Value::as_table)
+            .filter_map(|order| {
+                if let (Some(Value::String(item)), Some(Value::Integer(quantity))) =
+                    (order.get("item"), order.get("quantity"))
+                {
+                    Some(Order {
+                        item: item.to_owned(),
+                        quantity: *quantity as u32,
+                    })
+                } else {
+                    None
+                }
+            })
+            .collect();
+        Some(orders)
     }
 }
